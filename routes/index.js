cb79ed789e543d633507831eabdefeae8f005f50
Debargha12 <ghoshdebargha2524@gmail.com>
2020-07-29 00:59:51 -0500

    cart created

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
-//var Cart = require('../models/cart');
+var Cart = require('../models/cart');
 var csrf = require('csurf');
 const passport = require('passport');
 
@@ -21,6 +21,20 @@ router.get('/', function (req, res, next) {
             productChunks.push(docs.slice(i, i + chunkSize));
         }
         res.render('shop/index', {title: 'Shopping Cart', products: productChunks});
+    });
+});
+router.get('/add-to-cart/:id', function(req, res, next) {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+    Product.findById(productId, function(err, product) {
+        if (err) {
+            return res.redirect('/');
+        }
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        console.log(req.session.cart);
+        res.redirect('/');
     });
 });
 
